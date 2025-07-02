0450e7126553f53d0730286abb1f55773b16319d
MutuaNdunda <datascience@ndunda.com>
2025-07-02 20:31:03 +0300

    Added values

--- app.py
+++ app.py
@@ -44,5 +44,7 @@ def explain():
         return f"An error occurred: {e}"
 
 if __name__ == '__main__':
-    print("✅ Flask app running at http://127.0.0.1:5000/")
-    app.run(debug=True)
+    print("✅ Flask app running at http://127.0.0.1:5050/")
+    app.run(host="0.0.0.0", port=5050, debug=True)
+
+
